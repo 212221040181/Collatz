6fe17c930456c8861bb7747d010efb45432c2488
Vishnu priya <vishnupriya7299549305@gmail.com>
2023-08-24 10:30:31 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
+public class Main
+{
+    public static void printCollatzSequence(int n) {
+        while (n != 1) {
+            System.out.print(n + " ");
+
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = 3 * n + 1;
+            }
+        }
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        printCollatzSequence(n);
+
+        scanner.close();
+    }
+}
